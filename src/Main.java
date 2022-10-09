4ff95ff5f6166ad17d15684b266e153c0ac7e7b7
Reman Daniel <dani_reman@yahoo.com>
2022-10-09 23:56:09 +0300

    actualizare

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        out.println("Exercitiul 1: \n");
+        out.println("Hello\nDaniel\n");
+
+
+        System.out.println("Exercitiul 2: \n");
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        System.out.println(c);
+
+
+        System.out.println("Exercitiul 3: \n");
+        float d = 3;
+        double e = 10;
+        double f = e/d;
+        System.out.println(f);
+
+        out.println("Exercitiul 4: \n");
+        int g=  -5+8*6;
+        out.println(g);
+        int h = (55 + 9) % 9;
+        out.println(h);
+
+
+
+
     }
 }
